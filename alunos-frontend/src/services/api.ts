bb7a97f77524c9e5bf4a7d879c82e82d4110ddd2
EdmarMagalhaes <edmarpmc@gmail.com>
2025-04-30 20:03:55 -0300

    Configura VITE_API_BASE e ajusta API para produção

--- alunos-frontend/src/services/api.ts
+++ alunos-frontend/src/services/api.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080', // Removido o prefixo /api
+  baseURL: import.meta.env.VITE_API_BASE, // para Vite
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
 
 export default api;
